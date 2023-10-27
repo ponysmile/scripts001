f5b69dd78c6ad41ce9fb3d08d96e82cb463f2bc5
ponysmile <49753054+ponysmile@users.noreply.github.com>
2023-10-27 10:31:47 +0800

    Update venn.R

--- venn.R
+++ venn.R
@@ -5,13 +5,13 @@ A=a1$symbol
 B=a2$symbol
 venn.plot <- venn.diagram(
   list(A=A,B=B),
-  filename = "apoptosis.tiff",##韦恩图的名字
+  filename = "apoptosis.tiff",
   lty = 1,
   lwd = 1,
-  col = "black",  ##圈的颜色
-  fill = c("coral", "cornflowerblue"),##对应每个圈的颜色，有几个数据集，就需要有相应数量的颜色
+  col = "black", 
+  fill = c("coral", "cornflowerblue"),
   alpha = 0.60,
-  cat.col = "black",##此处设置每个数据集的名称颜色，也可以使用c（）函数输入三种颜色
+  cat.col = "black",
   cat.cex = 0.8,
   cat.fontface = "bold",
   margin = 0.07,
